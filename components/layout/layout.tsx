4002ff14160b5e5f45ecdf97c8d38c658a23bcd5
Benneth-Bejamas <benneth@bejamas.io>
2022-04-30 14:20:18 +0100

    resolving sass import error by adding prependData to next.config

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,20 +1,23 @@
 import React, { ReactNode } from "react";
-import Link from "next/link";
+import * as styles from "./layout.module.scss";
 import Head from "next/head";
+import Sidebar from "../sidebar/sidebar";
 
 type Props = {
   children?: ReactNode;
   title?: string;
 };
 
-const Layout = ({ children, title = "This is the default title" }: Props) => (
-  <div>
+const Layout = ({ children, title = "Welcome " }: Props) => (
+  <div className={styles.wrapper}>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>hey</header>
+    <header>
+      <Sidebar />
+    </header>
     {children}
     <footer>
       <hr />
---
+++ components/sidebar/sidebar.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as styles from "./sidebar.module.scss";
+
+const Sidebar = () => {
+  return <div className={styles.wrapper}>Sidebar </div>;
+};
+
+export default Sidebar;
